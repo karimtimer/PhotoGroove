e2c9538fb6658c971e3286fb3c2c0c5595bc338f
karimtimer <abdikarimtimer@gmail.com>
2021-03-26 16:51:19 +0000

    splitting out viewThumbnail

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,27 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
-view model =
-    div [ class "content" ]
-        [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
-        ]
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
 
 
 main =
-    view "no model yet"
+    view initialModel
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        ]
+
+
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
